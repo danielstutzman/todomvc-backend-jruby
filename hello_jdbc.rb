ee09fec22b6d63226d4091fe8152cff260f6b0a2
Dan Stutzman <dan@danstutzman.com>
2016-08-31 14:00:15 -0600

    Added web server with /todos route.

--- hello_jdbc.rb
+++ hello_jdbc.rb
@@ -1,15 +1,38 @@
-Java::org.postgresql.Driver
-conn = java.sql.DriverManager.get_connection(
-  'jdbc:postgresql://localhost:5432/todomvc', 'postgres', 'postgres')
-sql = 'SELECT id, title, completed FROM todo_items ORDER BY id'
-stmt = conn.createStatement
-rset = stmt.executeQuery sql 
-while rset.next
-  id = rset.getInt(1)
-  title = rset.getString(2)
-  completed = rset.getBoolean(3)
-  puts sprintf("Todo(%d,'%s',%s)", id, title, completed)
+require 'json'
+require 'webrick'
+
+class TodosServlet < WEBrick::HTTPServlet::AbstractServlet
+  def get_instance(server)
+    self
+  end
+  def initialize(conn)
+    @conn = conn
+  end
+  def do_GET request, response
+    todos = []
+    stmt = @conn.createStatement
+    rset = stmt.executeQuery 'SELECT id, title, completed FROM todo_items ORDER BY id'
+    while rset.next
+      todos.push({
+        id:        rset.getInt(1),
+        title:     rset.getString(2),
+        completed: rset.getBoolean(3),
+      })
+    end
+    rset.close
+    stmt.close
+
+    response.status = 200
+    response['Content-Type'] = 'application/json'
+    response.body = todos.to_json
+  end
 end
-rset.close
-stmt.close
-conn.close
+
+Java::org.postgresql.Driver
+conn = java.sql.DriverManager.get_connection \
+  'jdbc:postgresql://localhost:5432/todomvc', 'postgres', 'postgres'
+
+server = WEBrick::HTTPServer.new Port: 8000
+server.mount '/todos', TodosServlet.new(conn)
+trap 'INT' do server.shutdown end
+server.start
--- nailgun_client.sh
+++ nailgun_client.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -ex
+
+# Forward Ctrl-C to nailgun server
+trap 'ps aux | grep java | grep nailgun | awk "{print \$2}" | xargs kill -s INT' INT
+
 time jruby --ng hello_jdbc.rb
